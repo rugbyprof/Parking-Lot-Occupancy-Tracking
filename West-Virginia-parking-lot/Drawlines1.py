081781d8d98d4d986b51a245a881d77a9612c174
Kevin Ellis <kellis224@yahoo.com>
2016-09-20 13:23:49 -0500

    Add files via upload

---
+++ West-Virginia-parking-lot/Drawlines1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2
+import json
+
+
+with open('parking1test.json') as data_file:
+    data = json.load(data_file)
+
+# Create a black image
+#img = np.zeros((512,512,3), np.uint8)
+img = cv2.imread('Firstlinetest2.jpg')
+
+# Draw a diagonal blue line with thickness of 5 px
+#cv2.line(img,(0,0),(511,511),(255,0,0),1)
+
+# for letter,points in data.items():
+#     print(points[0])
+#     print(points[1])
+#
+#     cv2.line(img,(int(points[0]['x']),int(points[0]['y'])),(int(points[1]['x']),int(points[1]['y'])),(255,0,0),1)
+
+for i in range(0,27): #Change for how many spots
+    print(chr(65+i))
+    cv2.line(img,(int(data[chr(65+i)][0]['x']),int(data[chr(65+i)][0]['y'])),(int(data[chr(65+i)][1]['x']),int(data[chr(65+i)][1]['y'])) # 1 -> 2
+,(255,0,0),1)
+    cv2.line(img,(int(data[chr(65+i)][0]['x']),int(data[chr(65+i)][0]['y'])),(int(data[chr(65+i)][2]['x']),int(data[chr(65+i)][2]['y'])) #1 ->3
+,(255,0,0),1)
+    cv2.line(img,(int(data[chr(65+i)][1]['x']),int(data[chr(65+i)][1]['y'])),(int(data[chr(65+i)][3]['x']),int(data[chr(65+i)][3]['y'])) #2 -> 4
+,(255,0,0),1)
+    cv2.line(img,(int(data[chr(65+i)][2]['x']),int(data[chr(65+i)][2]['y'])),(int(data[chr(65+i)][3]['x']),int(data[chr(65+i)][3]['y'])) #3 -> 4
+,(255,0,0),1)
+
+# i = 7
+# print(chr(65+i))
+#
+#cv2.line(img,(int(data[chr(65+i)][0]['x']),int(data[chr(65+i)][0]['y'])),(int(data[chr(65+i)][1]['x']),int(data[chr(65+i)][1]['y'])),(
+#255,0,0),1)
+
+cv2.imwrite('out.jpg',img)
